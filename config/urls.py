b7b1b835368f6f673624cde5df3d9df30d7fb709
변윤성 <may670@naver.com>
2023-09-30 17:00:21 +0900

    main제목변경

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/',include('main.urls')),
+    path('',include('main.urls')),
 ]
 
 handler404 = 'main.views.page_not_found'
